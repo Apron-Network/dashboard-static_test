7a1ad0a75dc01e18bf0bf75cd283d08d1083a0f4
srdothunter <77256808+srdothunter@users.noreply.github.com>
2021-07-29 12:17:05 +0800

    add config.js

---
+++ js/config.js
@@ -0,0 +1,102 @@
+window.config = {
+        type:{
+            "list": [
+                {
+                    "name": "Heco",
+                    "img": "./images/Heco_Logo_Vi.svg",
+                    "value": 1,
+                    "imgHover": "./images/Heco_Logo_White.svg",
+                    "desc": "Instant access to the Huobi Echo Chain network with low latency and community supports."
+                },
+                {
+                    "name": "Bsc",
+                    "img": "./images/Logo_Bsc_Vi_H64.svg",
+                    "value": 2,
+                    "imgHover": "./images/binance_logo_White.svg",
+                    "desc": "Instant access to the Binance Super Chain network with low latency and community supports."
+                },
+                {
+                    "name": "Mina",
+                    "img": "./images/mina_hover.png",
+                    "value": 3,
+                    "imgHover": "./images/mina.png",
+                    "desc": "Instant access to the Mina Blockchain with low latency and community supports."
+                },
+                // {
+                //     "name": "Cosmos",
+                //     "img": "./images/Cosmos_hover.png",
+                //     "value": 4,
+                //     "imgHover": "./images/Cosmos.png",
+                //     "desc": "Instant access to the Cosmos Network with low latency and community supports."
+                // },
+                // {
+                //     "name": "Kusama",
+                //     "img": "./images/kusama_hover.png",
+                //     "value": 5,
+                //     "imgHover": "./images/kusama.png",
+                //     "desc": "Instant access to the Kusama Network with low latency and community supports."
+                // },
+                // {
+                //     "name": "Polygon",
+                //     "img": "./images/polygon_hover.png",
+                //     "value": 6,
+                //     "imgHover": "./images/polygon.png",
+                //     "desc": "Instant access to the Polygon Network with low latency and community supports."
+                // },
+                // {
+                //     "name": "SifChain",
+                //     "img": "./images/sifchain_hover.png",
+                //     "value": 7,
+                //     "imgHover": "./images/sifchain.png",
+                //     "desc": "Instant access to the SifChain Network with low latency and community supports."
+                // },
+                // {
+                //     "name": "Polkadot",
+                //     "img": "./images/polkadot_hover.png",
+                //     "value": 8,
+                //     "imgHover": "./images/polkadot.png",
+                //     "desc": "Instant access to the Polkadot Network with low latency and community supports."
+                // }
+            ]
+
+
+        },
+        news:[
+            // {
+            //     date: "July 28，2021",
+            //     name: "We are gladly to announce that our Polkadot Mainnet RPC service is Live."
+            // },
+            // {
+            //     date: "July 28，2021",
+            //     name: "We are gladly to announce that our SifChain Mainnet RPC service is Live."
+            // },
+            // {
+            //     date: "July 28，2021",
+            //     name: "We are gladly to announce that our Polygon Mainnet RPC service is Live."
+            // },
+            // {
+            //     date: "July 28，2021",
+            //     name: "We are gladly to announce that our Kusama Mainnet RPC service is Live."
+            // },
+            // {
+            //     date: "July 28，2021",
+            //     name: "We are gladly to announce that our Cosmos Mainnet RPC service is Live."
+            // },
+            {
+                date: "July 28，2021",
+                name: "We are gladly to announce that our Mina Mainnet RPC service is Live."
+            },
+            {
+                date: "May 13，2021",
+                name: "We are gladly to announce that our BSC Mainnet RPC service is Live."
+            },
+            {
+                date: "May 13，2021",
+                name: "We are gladly to announce that our Heco Testnet RPC service is Live."
+            },
+            {
+                date: "May 13，2021",
+                name: "We are gladly to announce that our Heco Mainnet RPC service is Live."
+            }
+        ]
+}
